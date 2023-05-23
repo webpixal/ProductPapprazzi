a86bc3e129f5d0aa989e3f5766cfe793b376341d
KH Nazmul Ahamed <khnazmulahamedbd@gmail.com>
2023-05-24 04:17:18 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <h2 className='text-red-900 font-bold' >Home</h2>
+  )
+}
