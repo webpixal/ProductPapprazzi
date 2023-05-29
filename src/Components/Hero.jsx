25a04dda359746824b75fa82acde4b0b9565bfb0
KH Nazmul Ahamed <khnazmulahamedbd@gmail.com>
2023-05-29 14:16:54 +0600

    About Section complet

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,40 +1,46 @@
 import React from 'react'
 import Link from 'next/link';
+import { Facebook, Line, Linkdin, Twitter } from '@/Assets/SVGs/SVG';
 
 const Hero = () => {
   return (
-    <div className="relative flex justify-start items-center">
-          <div className=' 2xl:container md:mx-auto'>
-            <div className='w-[641px] h[77px] text-white'>
-            <h3 className='font-light track'>Captivate</h3>
-            <h2>Your Audience at First Glance</h2>
-            <p>Exceptional product photography shapes consumers' crucial initial<br/>
+    <div className="relative flex justify-center items-center">
+           <div className="absolute mx-auto right-16 z-50"> 
+              <span >
+              <Line className='mb-[49px] ml-3'/>
+              </span>
+              <span>
+              <Facebook className='mb-[49px] hover:bgone'/>
+              <Linkdin className='mb-[49px] '/>
+              <Twitter className='mb-[49px] '/>
+              </span>
+              <span>
+              <Line className='mb-[49px] ml-3'/>
+              </span>
+            </div>
+          <div className=' 2xl:container md:mx-auto z-50'>
+            <div className=' w-[642px] h[77px] text-white'>
+            <h3 className='font-light tracking-[.35em]'>Captivate</h3>
+            <h2 className='text-5xl font-bold mt-[7px]'>Your Audience at First Glance</h2>
+            <p className='text-xl font-light mt-[14px]'>Exceptional product photography shapes consumers' crucial initial<br/>
               impressions, compelling them to explore and engage with your brand further.
             </p>
-          <Link href='/deshboard'>
-            <button className='relative bgone w-[123px] h-[48px] text-white rounded-full 
-            before:content-[""] before:absolute before:w-[100%] 
-            before:h-[100%] before:bgtwo before:rounded-full
-            before:top-0 before:left-0 before:scale-x-0 before:transition-transform 
-            before:ease-in before:origin-right before:duration-500
-            hover:before:scale-x-100 hover:before:origin-left hover:before:rounded-full 
-            hover:before:ease-in 
-            '><span className='z-50 relative'>
-              Read More
-              </span>
-              </button>
+              <Link href='/deshboard'>
+                <button className=' bgone link w-[123px] h-[48px] text-white mt-[31px]'>
+                    <span className='z-50 relative'>
+                    Read More
+                    </span>
+                  </button>
               </Link>
             </div>
           </div>
-        <div className="  bg-black bg-no-repeat h-[87vh] bg-center bg-cover  bg-blend-overlay">
-          <video autoPlay loop muted className="absolute inset-0 object-cover h-[87vh] xl:h-[87vh] w-full z-[-1]">
-            <source
-              src="Videos/bg.mp4"
-              type="video/mp4"
-            />
-          </video>
-          
-
+          <div className="video-docker bg-no-repeat h-[86.6vh] bg-center bg-cover  bg-blend-overlay">
+              <video autoPlay loop muted className=" absolute inset-0 object-cover mt-[-131px] h-[100vh] xl:h-[100vh] w-full z-[-1]">
+                <source
+                  src="Videos/bg1.mp4"
+                  type="video/mp4"
+                />
+              </video>         
         </div>
       </div>
   )
--- src/Components/aboutus.jsx
+++ src/Components/aboutus.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
     <div className='mt-[92px] mb-[92px]'>
           <div className='2xl:container md:mx-auto'>
             <div className=' flex justify-between items-center h-[473px]'>
-              <div className='w-[648px]'>
+              <div className='w-[648px] delay-[300ms] duration-[600ms] taos:translate-x-[200px] taos:opacity-0' data-taos-offset="400">
                 {/* About Content */}
                 <Badge className='mb-[24px]'/>
                 <h1 className='text-5xl font-bold capitalize leading-[60px]'>Capturing the 
@@ -31,6 +31,20 @@ const AboutUs = () => {
               </div>
               
             </div> 
+            <div className='flex justify-between items-center h-[110px] mt-[28px]'>
+                <div className=' w-[421px]'>
+                  <h4 className='clip text-xl font-semibold'>Our Expertise</h4>
+                  <p className='text-base font-extralight'>As specialists in product photography, we understand that the perfect image is more than just a high-resolution photograph against a white background. </p>
+                </div>
+                <div className=' w-[390px]'>
+                  <h4 className='clip text-xl font-semibold'>Turning Product Features into Benefits</h4>
+                  <p className='text-base font-extralight'>Our talented designers transform mundane product photographs into compelling visual stories through the power of infographics.</p>
+                </div>
+                <div className=' w-[370px]'>
+                  <h4 className='clip text-xl font-semibold'>Bringing Your Products into the Real World</h4>
+                  <p className='text-base font-extralight'>Our lifestyle photography service adds a touch of authenticity to your product images by placing them in real-life situations.</p>
+                </div>
+            </div>
         </div>
     </div>
   )
