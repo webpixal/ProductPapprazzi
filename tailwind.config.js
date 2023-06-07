d4f1197cb6c8b21a5e28be61635dbcaad83a1b69
KH Nazmul Ahamed <khnazmulahamedbd@gmail.com>
2023-06-07 21:58:32 +0600

    biuld

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  purge: [
-    "./src/components/**/*.{js,jsx}",
-    "./public/index.html",
-  ],
+	"./src/pages/**/*.{js,ts,jsx,tsx}",
+	"./src/components/**/*.{js,ts,jsx,tsx}"
+],
   theme: {
     extend: {
       backgroundImage: {
